bd9964ee2915922e8f8a53715c7a2e07c0510fe5
tarath01 <epstrath01@gmail.com>
2026-04-28 15:43:06 -0500

    Initial commit

---
+++ public/app.js
@@ -0,0 +1,31 @@
+/**
+ * @author Taylor Rath
+ * @class Trivia
+ * @returns {Promise<void>}
+ */
+
+async function loadQuestion() {
+    const status = document.getElementById("status");
+    const questionEl = document.getElementById("question");
+
+    try {
+        status.textContent = "Loading...";
+        const response = await fetch(`/api`);
+
+        if (!response.ok) {
+            throw new Error("Trivia not found!");
+        }
+
+        const data = await response.json();
+        console.log(data);
+
+        // Populate the card
+        questionEl.textContent = data.question;
+        status.textContent = "";
+
+    } catch (error) {
+        questionEl.textContent = "Error: " + error.message;
+    }
+}
+// Allow pressing Enter to search
+            loadQuestion();
---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/api",async (req, res) => {
+    try {
+
+        const response = await fetch(`https://opentdb.com/api.php?amount=1&category=9&type=boolean`);
+
+        if (!response.ok) {
+            return res.status(404).json({ error: "Question not found" });
+        }
+
+        const data = await response.json();
+
+        // Send back only the trivia we need
+        res.json({
+            question: data.results[0].question,
+            correct_answer: data.results[0].correct_answer,
+        });
+
+    } catch (error) {
+        res.status(500).json({ error: "Server error: " + error.message });
+    }
+});
+
+app.listen(3000, () => {
+    console.log("Server running on http://localhost:3000");
+});
