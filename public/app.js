47fb19b6cb759a44e236ebdc9ce48a4c059aa62b
tarath01 <epstrath01@gmail.com>
2026-04-28 15:58:11 -0500

    Radio

--- public/app.js
+++ public/app.js
@@ -3,7 +3,11 @@
  * @class Trivia
  * @returns {Promise<void>}
  */
+let correctAnswer;
 
+function checkAnswer() {
+
+}
 async function loadQuestion() {
     const status = document.getElementById("status");
     const questionEl = document.getElementById("question");
@@ -21,6 +25,7 @@ async function loadQuestion() {
 
         // Populate the card
         questionEl.textContent = data.question;
+        correctAnswer = data.correct_answer;
         status.textContent = "";
 
     } catch (error) {
